4f0acaf487fd07ec98c77b0627301d0dad2393c3
Life Savor CI <ci@lifesavor.ai>
2026-05-04 01:13:52 +0000

    Update lsai-cli to v0.1.49

--- Formula/lsai-cli.rb
+++ Formula/lsai-cli.rb
@@ -1,13 +1,13 @@
 class LsaiCli < Formula
   desc "Life Savor AI Developer CLI"
   homepage "https://lifesavor.ai"
-  version "0.1.48"
+  version "0.1.49"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://download.dev.lifesavor.ai/lsai-cli/v0.1.48/lsai-cli-v0.1.48-aarch64-apple-darwin.tar.gz"
-      sha256 "210df77218ce032fd2dfe8bd586d98472e529f94ce829fba38a021bcf0b0c5d7"
+      url "https://download.dev.lifesavor.ai/lsai-cli/v0.1.49/lsai-cli-v0.1.49-aarch64-apple-darwin.tar.gz"
+      sha256 "418887d2cc9e22ac185e2091620b5ce17333244072ef211241f268d0af390254"
     end
   end
 
